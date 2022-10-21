fda53a2b0465d6fb2bf3158519cf754417041c1e
DaisyChepyegon <daisychepyegon@gmail.com>
2022-10-21 18:19:45 +0300

    add seed data

--- db/migrate/20221021142322_create_route_plans.rb
+++ db/migrate/20221021142322_create_route_plans.rb
@@ -3,7 +3,6 @@ class CreateRoutePlans < ActiveRecord::Migration[7.0]
     create_table :route_plans do |t|
       t.string :name
       t.string :month
-      t.references :manager, null: false, foreign_key: true
 
       t.timestamps
     end
