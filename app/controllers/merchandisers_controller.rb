31c03dca0abb5a7b8a0d39031e3c0743ef45ff8f
DaisyChepyegon <daisychepyegon@gmail.com>
2022-10-21 17:34:53 +0300

    add all the controller actions

--- app/controllers/merchandisers_controller.rb
+++ app/controllers/merchandisers_controller.rb
@@ -1,51 +1,69 @@
 class MerchandisersController < ApplicationController
   before_action :set_merchandiser, only: %i[ show update destroy ]
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  # GET /merchandisers
-  def index
-    @merchandisers = Merchandiser.all
+   # GET /merchandisers
+   def index
+    merchandisers = Merchandiser.all
 
-    render json: @merchandisers
+    render json: merchandisers
   end
 
   # GET /merchandisers/1
   def show
-    render json: @merchandiser
+    merchandiser = set_merchandiser
+    render json: merchandiser
+   
   end
 
   # POST /merchandisers
   def create
-    @merchandiser = Merchandiser.new(merchandiser_params)
 
-    if @merchandiser.save
-      render json: @merchandiser, status: :created, location: @merchandiser
+    merchandiser = Merchandiser.create!(merchandiser_params)
+    if  merchandiser.valid?
+        session[:admin_id] =  merchandiser.id
+        render json: {
+            status: :created,
+            merchandiser: merchandiser
+        }
     else
-      render json: @merchandiser.errors, status: :unprocessable_entity
+        render json: {
+            status: 500,
+            errors: merchandiser.errors.full_messages
+        }
     end
   end
 
+
+
+
   # PATCH/PUT /merchandisers/1
   def update
-    if @merchandiser.update(merchandiser_params)
-      render json: @merchandiser
-    else
-      render json: @merchandiser.errors, status: :unprocessable_entity
-    end
+    merchandiser = set_merchandiser
+      merchandiser.update!(merchandiser_params)
+      render json: merchandiser
+    
   end
 
   # DELETE /merchandisers/1
   def destroy
-    @merchandiser.destroy
+    merchandiser = set_merchandiser
+      merchandiser.destroy
+      head :no_content
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_merchandiser
-      @merchandiser = Merchandiser.find(params[:id])
+     Merchandiser.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def merchandiser_params
-      params.require(:merchandiser).permit(:username, :image, :phone_number, :email, :password_digest, :location, :route_plan_id)
+      params.require(:merchandiser).permit(:username, :email,  :password, :user_id, :password_confirmation)
     end
+
+    def render_not_found_response
+      render json: { error: "Merchandiser not found" }, status: :not_found
+     end
 end
--- app/controllers/outlets_controller.rb
+++ app/controllers/outlets_controller.rb
@@ -1,51 +1,55 @@
 class OutletsController < ApplicationController
   before_action :set_outlet, only: %i[ show update destroy ]
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  # GET /outlets
-  def index
-    @outlets = Outlet.all
+   # GET /outlets
+   def index
+    outlets = Outlet.all
+ 
+     render json: outlets
+   end
+ 
+   # GET /outlets/1
+   def show
+     outlet = Outlet.find(params[:id])
+       render json: outlet
+   end
+ 
+   # POST /outlets
+   def create
+    outlet = Outlet.create!(outlet_params)
+    render json: outlet, status: :created
+ 
+   end
+ 
+   # PATCH/PUT /outlets/1
+   def update
+     outlet = set_outlet
+       outlet.update!(outlet_params)
+       render json:outlet
+     
+   end
+ 
+   # DELETE /outlets/1
+   def destroy
+     outlet = set_outlet
+       outlet.destroy
+       head :no_content
+    
+   end
+ 
+   private
+     # Use callbacks to share common setup or constraints between actions.
+     def set_outlet
+      Outlet.find(params[:id])
+     end
+ 
+     # Only allow a list of trusted parameters through.
+     def outlet_params
+       params.require(:outlet).permit(:name, :location, :merchandiser_id)
+     end
 
-    render json: @outlets
-  end
-
-  # GET /outlets/1
-  def show
-    render json: @outlet
-  end
-
-  # POST /outlets
-  def create
-    @outlet = Outlet.new(outlet_params)
-
-    if @outlet.save
-      render json: @outlet, status: :created, location: @outlet
-    else
-      render json: @outlet.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /outlets/1
-  def update
-    if @outlet.update(outlet_params)
-      render json: @outlet
-    else
-      render json: @outlet.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /outlets/1
-  def destroy
-    @outlet.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_outlet
-      @outlet = Outlet.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def outlet_params
-      params.require(:outlet).permit(:name, :location, :route_plan_id, :merchandiser_id)
-    end
+     def render_not_found_response
+      render json: { error: "Outlet not found" }, status: :not_found
+     end
 end
