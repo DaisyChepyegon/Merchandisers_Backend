acba0c4b9965aadb60dd3fe745b8642641fd7b67
jackline chepngeno <chepngenojackline0@gmail.com>
2022-10-22 11:03:30 +0300

    minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
 
