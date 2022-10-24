0ec033b1afc33ed662eb63d2fd9489317c6470e5
jackline chepngeno <chepngenojackline0@gmail.com>
2022-10-24 12:17:02 +0300

    change manager session controller

--- app/controllers/managers_sessions_controller.rb
+++ app/controllers/managers_sessions_controller.rb
@@ -1,46 +1,29 @@
 class ManagersSessionsController < ApplicationController
     def create
-      manager = Manager.find_by(email: params[:email])
-              
-      if manager &.authenticate(params[:password])
-          session[:manager_id] = manager.id
-          
-          # render json: manager, status: :created
-          app_response(message: "Log in success", body: manager)
+      manager = Manager.find_by(email: sessions_params[:email])
+      if manager &.authenticate(sessions_params[:password])
+        create_manager_session(manager.id)
+        app_response(message: "Log in success", body: manager)
       else
           app_response(status_code: 401, message: "Invalid username or password")
       end
     end
     
-      def is_logged_in?
-        if logged_in_manager? && current_manager
-          render json: {
-            logged_in: true,
-            manager: current_manager
-          }
-        else
-          render json: {
-            logged_in: false,
-            message: 'no such Manager'
-          }
-        end
-      end
     
-      def destroy
+    def destroy
         delete_manager_session
-        render json: {
-            status: 200,
-            logged_out: true
-            # message: 'Log out successfully'
-        }
+        app_response(status_code: 200, message: "Log out successfully")
       end
     
       private
-      def session_params
-        params.permit(:email, :password)
-    end
+      def create_manager_session(manager.id)
+        session[:manager_id] ||= manager_id
+      end
 
       def delete_manager_session
         session.delete :manager_id
       end
+      def sessions_params
+        params.require(:manager).permit(:username, :email, :password)
+    end
 end
