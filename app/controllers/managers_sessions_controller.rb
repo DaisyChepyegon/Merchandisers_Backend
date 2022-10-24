88dc0a3c41ae2fe25bc5969299c329b3f668fbc7
jackline chepngeno <chepngenojackline0@gmail.com>
2022-10-24 13:42:41 +0300

    remove method

--- app/controllers/managers_sessions_controller.rb
+++ app/controllers/managers_sessions_controller.rb
@@ -2,7 +2,7 @@ class ManagersSessionsController < ApplicationController
     def create
       manager = Manager.find_by(email: sessions_params[:email])
       if manager &.authenticate(sessions_params[:password])
-        create_manager_session(manager_id)
+        session[:manager_id] ||= manager_id
         app_response(message: "Log in success", body: manager)
       else
           app_response(status_code: 401, message: "Invalid username or password")
@@ -16,9 +16,6 @@ class ManagersSessionsController < ApplicationController
       end
     
       private
-      def create_manager_session(manager_id)
-        session[:manager_id] ||= manager_id
-      end
 
       def delete_manager_session
         session.delete :manager_id
