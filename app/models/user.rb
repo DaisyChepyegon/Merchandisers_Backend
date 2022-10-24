6e0a4b40b48e63f5d72e2b57c9c8673427f44413
DaisyChepyegon <daisychepyegon@gmail.com>
2022-10-24 18:03:10 +0300

    validate user email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ApplicationRecord
     #validates the password
+    before_validation :downcase_email
+
+    def downcase_email
+        email.try(:downcase!)
+    end
+
+    validates :username, presence: true, uniqueness: true
+    #validates email
+    validates :email, presence: true, uniqueness: true
+    validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+
+    #validates the password
     has_secure_password
     validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
     validates :password, confirmation: true
