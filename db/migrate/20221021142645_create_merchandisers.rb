2357a5c3f22ccf29ea735b51e21fcc9ed9660b69
DaisyChepyegon <daisychepyegon@gmail.com>
2022-11-01 12:26:11 +0300

    rename an attribute

--- db/migrate/20221021142645_create_merchandisers.rb
+++ db/migrate/20221021142645_create_merchandisers.rb
@@ -4,7 +4,7 @@ class CreateMerchandisers < ActiveRecord::Migration[7.0]
       t.string :username
       t.string :image
       t.string :role
-      t.integer :phone_number
+      t.integer :contact
       t.string :email
       t.string :password_digest
       t.string :location
